fb969aac8052c3691c89fb03d5fcb2d115c96710
ah-mobil <ah-mobil@naxos.istec.de>
2013-08-12 22:40:14 +0200

    local config

--- anmeldung/config.inc.php
+++ anmeldung/config.inc.php
@@ -1,14 +1,8 @@
 <?php
 
-/*
 $host="localhost";
 $user="root";
 $password="";
-$database="anmeldung";
-*/
-$host="rdbms.strato.de";
-$user="U1226375";
-$password="qwertz88";
-$database="DB1226375";
+$database="turnieranmeldung";
 
 ?>
