824c436fa4f84cf307fad69b306284c548435247
ah-mobil <ah-mobil@naxos.istec.de>
2013-08-27 21:48:48 +0200

    einheitl. Tab-layout, btn-mini

--- anmeldung/vereine.php
+++ anmeldung/vereine.php
@@ -10,15 +10,14 @@ $conn->PConnect($host,$user,$password,$database);
 $ADODB_FETCH_MODE = ADODB_FETCH_ASSOC;
 
 unset($recordSet);
-//$sql='SELECT id, davor, name, kurz, passwort, region, ansprechpartner_email FROM tas_vereine ORDER BY name, davor';
 $sql='SELECT * FROM tas_vereine ORDER BY region, name, davor';
 $recordSet = &$conn->Execute($sql);
-$verein=$recordSet->getArray();
+$liste = $recordSet->getArray();
 $conn->Close();
 
 require('../smarty/libs/Smarty.class.php');
 $smarty = get_new_smarty();
-$smarty->assign('verein',$verein);
+$smarty->assign('liste',$liste);
 $smarty->assign('menuakt','vereine.php');
 $smarty->display('vereine.tpl.htm');
 ?>
