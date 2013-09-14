538d9e8115583db47d110cf0cf42673b8df1ca00
ahhess <andreas.h.hess@googlemail.com>
2013-09-14 22:45:40 +0200

    Anpassung wegen Server-PHP-Update

--- anmeldung/vereinskontakt.php
+++ anmeldung/vereinskontakt.php
@@ -4,6 +4,9 @@ include("config.inc.php");
 include("funktionen.inc.php");
 check_login();
 
+unset($systemmeldung);
+//$systemmeldung=$_SESSION["verein"]["id"]."-".$_SESSION["verein"]["davor"]."-".$_SESSION["verein"]["name"];
+
 if ($_POST["doSubmitEmail"])
 {
 	include("../adodb/adodb.inc.php");
@@ -24,19 +27,23 @@ if ($_POST["doSubmitEmail"])
 		ansprechpartner_mobil='".$_POST["ansprechpartner_mobil"]."', 
 		ansprechpartner_email='".$_POST["ansprechpartner_email"]."', 
 		ansprechpartner_bemerkung='".$_POST["ansprechpartner_bemerkung"]."' 
-		WHERE id=".$_SESSION["verein"]["id"];
-	$rs = &$conn->Execute($sql) or die ("Fehler beim Speichern. Bitte Administrator benachrichtigen.");
+		WHERE id=".$_POST["id"];
 
-	unset($rs);                                                                 // vereinsdaten neu laden für session
-	$sql='select * from tas_vereine where id='.$_SESSION["verein"]["id"];
+	$rs = &$conn->Execute($sql) or die ("Fehler beim Speichern. Bitte Administrator benachrichtigen.");
+	$systemmeldung='Daten wurden gespeichert.';
+
+	$sql='select * from tas_vereine where id='.$_POST["id"];
 	$rs = &$conn->Execute($sql);
 	$v=$rs->GetArray();
-	$_SESSION["verein"]=$v[0];
+	$v=$v[0];
+	$_SESSION["verein"]=$v;
 	$conn->Close();
 }
 
 require('../smarty/libs/Smarty.class.php');
 $smarty = get_new_smarty();
 $smarty->assign('menuakt','vereinskontakt.php');
+$smarty->assign('verein',$_SESSION["verein"]);
+$smarty->assign('systemmeldung',$systemmeldung);
 $smarty->display('vereinskontakt.tpl.htm');
 ?>
