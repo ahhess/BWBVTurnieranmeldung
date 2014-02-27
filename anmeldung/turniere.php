c56efb069cd9d497292f8ab0457909e425a0d196
ahhess <andreas.h.hess@googlemail.com>
2014-02-28 00:16:42 +0100

    multi-region auch fuer turniere

--- anmeldung/turniere.php
+++ anmeldung/turniere.php
@@ -11,12 +11,17 @@ $conn = &ADONewConnection('mysql');
 $conn->PConnect($host,$user,$password,$database);
 $ADODB_FETCH_MODE = ADODB_FETCH_ASSOC;
 
+$region = "NW";
+if ($_POST["region"])
+	$region = $_POST["region"];
+
 unset($recordSet);
-$sql='SELECT tas_turnier.*, tas_turnierbeauftragter.nachname, tas_turnierbeauftragter.vorname 
+$sql="SELECT tas_turnier.*, tas_turnierbeauftragter.nachname, tas_turnierbeauftragter.vorname 
 	FROM tas_turnier
 	LEFT OUTER JOIN tas_turnierbeauftragter 
 		ON tas_turnier.turnierbeauftragter_id = tas_turnierbeauftragter.id 
-	ORDER BY datum desc, name_lang';
+	WHERE tas_turnier.region = '$region'
+	ORDER BY datum desc, name_lang";
 $recordSet = &$conn->Execute($sql);
 $liste = $recordSet->getArray();
 $conn->Close();
@@ -26,5 +31,7 @@ $smarty = get_new_smarty();
 $smarty->assign('liste',$liste);
 $smarty->assign('menuakt','turniere.php');
 $smarty->assign('admin',$_SESSION["admin"]);
+$smarty->assign('region',$region);
+
 $smarty->display('turniere.tpl.htm');
 ?>
