b288e1d60f812a395ec940bccd0bcc2240f7f007
ahhess <andreas.h.hess@googlemail.com>
2016-06-25 09:11:59 +0200

    x

--- anmeldung/anzeige.php
+++ anmeldung/anzeige.php
@@ -1,72 +1,116 @@
 <?php
-// Anzeige der Spieleranmeldungen für ein bestimmtes Turnier
-session_start();
-include("config.inc.php");
-include("funktionen.inc.php");
-
-if ($_POST["admin"])
-	check_admin_login();
-else
+	// Anzeige der Spieleranmeldungen für ein bestimmtes Turnier
+	session_start();
+	include("funktionen.inc.php");
 	check_login();
 
-function spieler_markiert($id)
-{
-	print $id;
-	if (in_array($id,$_POST["meldung"])) return true;
-	else return false;
-}
 
-include("../adodb/adodb.inc.php");
-$conn = &ADONewConnection('mysql');	# create a connection
-//$conn->debug=true;
-$conn->PConnect($host,$user,$password,$database);
-$ADODB_FETCH_MODE = ADODB_FETCH_ASSOC;
+	//turnier holen
+	$sql="select * from tas_turnier where id=".$_GET["id"];
+	$recordSet = &$conn->Execute($sql);
+	$turniere=$recordSet->GetArray();
+	$turnier=$turniere[0];
 
-//turnier holen
-$sql="select * from tas_turnier where id=".$_GET["id"];
-$recordSet = &$conn->Execute($sql);
-$turniere=$recordSet->GetArray();
-$turnier=$turniere[0];
+	//meldungen holen
+	$sql='select tas_spieler.*, tas_vereine.davor, tas_vereine.name as verein, tas_meldung.partner as partner, tas_meldung.ak as ak 
+		from tas_meldung,tas_spieler,tas_vereine
+		where tas_meldung.spieler_id=tas_spieler.id 
+		and tas_meldung.turnier_id='.$_GET["id"].' 
+		and tas_meldung.verein_id=tas_vereine.id
+		order by tas_meldung.ak asc, tas_spieler.geschlecht asc, tas_vereine.name, tas_vereine.davor, tas_meldung.partner, tas_spieler.nachname';
+	$recordSetSpieler = &$conn->Execute($sql);
+	$meldungen=$recordSetSpieler->GetArray();
 
-//meldungen holen
-$sql='select tas_spieler.*, tas_vereine.davor, tas_vereine.name as verein, tas_meldung.partner as partner, tas_meldung.ak as ak 
-	from tas_meldung,tas_spieler,tas_vereine
-	where tas_meldung.spieler_id=tas_spieler.id 
-	and tas_meldung.turnier_id='.$_GET["id"].' 
-	and tas_meldung.verein_id=tas_vereine.id
-	order by tas_meldung.ak asc, tas_spieler.geschlecht asc, tas_vereine.name, tas_vereine.davor, tas_meldung.partner, tas_spieler.nachname';
-$recordSetSpieler = &$conn->Execute($sql);
-$meldungen=$recordSetSpieler->GetArray();
+	//vereine holen
+	$sqlV='SELECT tas_vereine.name, tas_vereine.davor, COUNT( * ) AS vmeldcount
+		FROM tas_meldung, tas_spieler, tas_vereine
+		WHERE tas_meldung.turnier_id='.$_GET["id"].'
+		AND tas_meldung.spieler_id = tas_spieler.id
+		AND tas_meldung.verein_id = tas_vereine.id
+		GROUP BY tas_vereine.name, tas_vereine.davor
+		ORDER BY tas_vereine.name, tas_vereine.davor';
+	$recordSetVereine = &$conn->Execute($sqlV);
+	$vereine=$recordSetVereine->GetArray();
 
-//vereine holen
-$sqlV='SELECT tas_vereine.name, tas_vereine.davor, COUNT( * ) AS vmeldcount
-	FROM tas_meldung, tas_spieler, tas_vereine
-	WHERE tas_meldung.turnier_id='.$_GET["id"].'
-	AND tas_meldung.spieler_id = tas_spieler.id
-	AND tas_meldung.verein_id = tas_vereine.id
-	GROUP BY tas_vereine.name, tas_vereine.davor
-	ORDER BY tas_vereine.name, tas_vereine.davor';
-$recordSetVereine = &$conn->Execute($sqlV);
-$vereine=$recordSetVereine->GetArray();
-//print "<pre>";print_r($meldungen);
 
-require('../smarty/libs/Smarty.class.php');
-$smarty = get_new_smarty();
-$smarty->caching = false;
-$smarty->assign('meldungen',$meldungen);
-$smarty->assign('countMeldungen',count($meldungen));
-$smarty->assign('vereine',$vereine);
-$smarty->assign('countVereine',count($vereine));
-$smarty->assign('turnier',$turnier);
-$smarty->assign('datum',date('d.m.Y'));
-$smarty->assign('zeit',date('G:i'));
-$smarty->assign('admin',$_SESSION["admin"]);
+	printHeader();
+?>		
+    <section id="turniere" class="container content-section text-center ">
+        <div class="row">
+            <div>
 
-if ($_GET["format"] == "csv")
-	$smarty->display('csv.tpl.htm');
-else
-	$smarty->display('anzeige2.tpl.htm');
-//print_r($meldungen);
+				<h2>Meldungen <?php echo $turnier['name_lang']; ?></h2>
+				
+				<p>am {$turnier.name_kurz} in {$turnier.ort}</p>
+				<p>Folgende Meldungen sind derzeit in dem Meldesystem hinterlegt.<br>
+				Hinweis: Nach Ende der Meldefrist kann diese Liste unter Umst&auml;nden nicht mehr aktuell sein. 
+				Kontaktieren Sie in diesem Fall ggf. den Turnierbeauftragten.</p>
+				<p>( Stand: <?php echo "$datum $zeit"; ?>  Uhr )</p>
 
-$conn->Close(); # optional
+				<h4>Bisher online gemeldete Vereine ( <?php echo count($vereine); ?>):</h4>
+				<p>
+				<?php 
+				for ($i = 0; $i < count($vereine); $i++) {
+					echo $vereine[$i]['davor']}." ".$vereine[$i]['name']." ".$vereine[$i]['vmeldcount']."<br>";
+				} 
+				?> 
+				</p>
+
+				<h4>Bisher gemeldete Spieler/innen ( <?php echo count($meldungen); ?>):</h4>
+
+				<table class="table table-hover table-condensed">
+					<thead>
+						<tr>
+							<th>Nachname</th>
+							<th>Vorname</th>
+							<th>Verein</th>
+							<th>Passnr.</th>
+							<th>Jahrgang</th>
+							<th>Altersklasse</th>
+							<th>Geschlecht</th>
+							<th>Partner</th>
+						</tr>
+					</thead>
+					<tbody>
+					<?php 
+					for ($spieler = 0; $spieler < count($meldungen); $spieler++) { ?>
+						<tr>
+							<td><?php echo $meldungen[$spieler]['nachname']; ?></td>
+							<td><?php echo $meldungen[$spieler]['vorname']; ?></td>
+							<td><?php echo $meldungen[$spieler]['davor']." ".$meldungen[$spieler]['verein']; ?></td>
+							<td><?php echo $meldungen[$spieler]['passnummer']; ?></td>
+							<td><?php echo $meldungen[$spieler]['geburtstag|date_format:"%Y"']; ?></td>
+							<td><?php echo $meldungen[$spieler]['ak']; ?></td>
+							<td><?php echo $meldungen[$spieler]['geschlecht']; ?></td>
+							<td><?php echo $meldungen[$spieler]['partner']; ?></td>
+						</tr>
+					<?php 
+					}
+					if ($meldungen[spieler.index_next]['ak'] ne $meldungen[spieler]['ak']) { 
+					<tr>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td>---</td>
+						<td>---</td>
+						<td></td>
+					</tr>
+					{elseif $meldungen[spieler.index_next].geschlecht ne $meldungen[spieler].geschlecht}
+					<tr>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td>---</td>
+						<td></td>
+					</tr>
+					{/if}
+				  {/section}
+				  </tbody>
+				</table>
+
 ?>
--- anmeldung/help.php
+++ anmeldung/help.php
@@ -1,9 +1,60 @@
 <?php
-session_start();
-include("funktionen.inc.php");
+	session_start();
+	include("funktionen.inc.php");
+	check_login();
+	printHeader();
+?>	
+<section id="faq" class="container content-section text-center ">
 
-require('../smarty/libs/Smarty.class.php');
-$smarty = get_new_smarty();
-$smarty->assign('menuakt','help.php');
-$smarty->display('help.tpl.htm');
+	<div class="row">
+        <div align="left" class="col-lg-10 col-lg-offset-1">
+
+			<h2>FAQ zum Turnieranmeldesystem</h2>
+
+			<dl>
+				<dt>Ich habe meine Zugangsdaten gerade erhalten und mich zum ersten Mal eingeloggt. Was ist nun zu tun?</dt>
+				<dd>Folgende zwei Schritte sind unbedingt durchzuf&uuml;hren:
+					<ul>
+						<li>Auf der Seite Vereinskontakt die Daten komplettieren/aktualisieren. 
+						Insbesonders die <em>Email-Adresse</em> muss stets aktuell gehalten werden! &Uuml;ber sie werden 
+						die Meldebest&auml;tigungen abgewickelt.
+						<li>Auf der Seite Spielerdaten die Spieler des Vereins einpflegen.
+					</ul>
+					Danach k&ouml;nnen Meldungen durchgef&uuml;hrt werden.
+					<br/>
+					<br/>
+				</dd>
+				
+				<dt>Ich erhalte keine Best&auml;tigungsmails. Woran kann das liegen??</dt>
+				<dd>Dies kann hpts. zwei Gr&uuml;nde haben:
+					<ul>
+						<li>Die bei Ansprechpartner angegebene <em>Email ist nicht korrekt</em> (&uuml;berpr&uuml;fen!!)
+						<li>Dein Email-Account besitzt einen <em>aktiviertem SPAM-Filter</em>? 
+						In diesem Fall ist im Ordner "SPAM", "Junk-Mail"  o.&auml;. nachzusehen. 
+						Sofern die Mail sich dort befindet, ist die Email als Nicht-SPAM zu kennzeichnen. 
+						Alle folgenden Emails sollten Dich dann ohne Probleme erreichen! 
+						Ansonsten bitte die Hilfe des Email Providers zu Rate ziehen.
+						<br/>
+						<br/>
+					</ul>
+				<dd>
+
+				<dt>Ich m&ouml;chte <em>an mehrere Email-Adressen</em> Best&auml;tigungen erhalten.</dt>
+				<dd>Auf der Seite <em>Vereinskontakt</em> k&ouml;nnen im E-Mail-Feld k&ouml;nnen mehrere Email-Adressen jeweils durch Strichpunkt/Semikolon (;) getrennt angegeben werden, z.B. <i>adresse1@email.de;adresse2@email.com;adresse3@email.biz</i>.
+				<br/>
+				<br/>
+				</dd>
+		
+				<dt>Bei sonstigen Fragen und Problemen: an wen soll ich mich wenden?</dt>
+				<dd>
+					<ul>
+						<li><em>Technische Fragen</em> werden von <a href="mailto:turnieradmin@bwbv.de">Andreas Hess, Sportwart NW</a> beantwortet.<li><em>Turnierbezogene Fragen</em> werden von dem jeweiligen ausrichtenden Verein bzw. dem Turnierverantwortlichen (Ranglistenbeauftragten etc.) beantwortet.
+					</ul>
+				<dd>
+			</dl>
+		</div>
+	</div>
+</section>
+<?php
+	printFooter();
 ?>
