30fcb64729e0c7a2dd11560be65cc941441691f2
ahhess <andreas.h.hess@googlemail.com>
2017-01-20 00:29:02 +0100

    meldeliste umgesetzt

--- anmeldung/anzeige.php
+++ anmeldung/anzeige.php
@@ -38,26 +38,26 @@
     <section id="turniere" class="container content-section text-center ">
         <div class="row">
             <div>
-
 				<h2>Meldungen <?php echo $turnier['name_lang']; ?></h2>
 				
-				<p>am {$turnier.name_kurz} in {$turnier.ort}</p>
-				<p>Folgende Meldungen sind derzeit in dem Meldesystem hinterlegt.<br>
-				Hinweis: Nach Ende der Meldefrist kann diese Liste unter Umst&auml;nden nicht mehr aktuell sein. 
+				<p>am <?php echo $turnier['name_kurz']; ?> in <?php echo $turnier['ort']; ?></p>
+				<p>Hinweis: Nach Ende der Meldefrist kann diese Liste unter Umst&auml;nden nicht mehr aktuell sein. 
 				Kontaktieren Sie in diesem Fall ggf. den Turnierbeauftragten.</p>
-				<p>( Stand: <?php echo "$datum $zeit"; ?>  Uhr )</p>
+				<p>( Stand: <?php echo date('d.m.Y')." ".date('G:i'); ?>  Uhr )</p>
 
 				<h4>Bisher online gemeldete Vereine ( <?php echo count($vereine); ?>):</h4>
-				<p>
-				<?php 
-				for ($i = 0; $i < count($vereine); $i++) {
-					echo $vereine[$i]['davor']}." ".$vereine[$i]['name']." ".$vereine[$i]['vmeldcount']."<br>";
-				} 
-				?> 
-				</p>
+				<table class="table table-hover table-condensed">
+					<tbody><?php 
+					for ($i = 0; $i < count($vereine); $i++) { ?>
+						<tr>
+							<td><?php echo $vereine[$i]['davor']." ".$vereine[$i]['name'];?></td>
+							<td><?php echo $vereine[$i]['vmeldcount']; ?></td>
+						</tr><?php
+					} ?> 
+					</tbody> 
+				</table>
 
 				<h4>Bisher gemeldete Spieler/innen ( <?php echo count($meldungen); ?>):</h4>
-
 				<table class="table table-hover table-condensed">
 					<thead>
 						<tr>
@@ -71,46 +71,47 @@
 							<th>Partner</th>
 						</tr>
 					</thead>
-					<tbody>
-					<?php 
-					for ($spieler = 0; $spieler < count($meldungen); $spieler++) { ?>
+					<tbody><?php 
+					for ($s = 0; $s < count($meldungen); $s++) { 
+						if ($s > 0) {
+							$s1 = $s-1;
+							if ($meldungen[$s1]['ak'] != $meldungen[$s]['ak']) { ?>
+							<tr>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td>---</td>
+								<td>---</td>
+								<td></td>
+							</tr><?php 
+							} else if ($meldungen[$s1]['geschlecht'] != $meldungen[$s]['geschlecht']) { ?>
+							<tr>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td>---</td>
+								<td></td>
+							</tr><?php 
+							}
+						} ?>
 						<tr>
-							<td><?php echo $meldungen[$spieler]['nachname']; ?></td>
-							<td><?php echo $meldungen[$spieler]['vorname']; ?></td>
-							<td><?php echo $meldungen[$spieler]['davor']." ".$meldungen[$spieler]['verein']; ?></td>
-							<td><?php echo $meldungen[$spieler]['passnummer']; ?></td>
-							<td><?php echo $meldungen[$spieler]['geburtstag|date_format:"%Y"']; ?></td>
-							<td><?php echo $meldungen[$spieler]['ak']; ?></td>
-							<td><?php echo $meldungen[$spieler]['geschlecht']; ?></td>
-							<td><?php echo $meldungen[$spieler]['partner']; ?></td>
-						</tr>
-					<?php 
-					}
-					if ($meldungen[spieler.index_next]['ak'] ne $meldungen[spieler]['ak']) { 
-					<tr>
-						<td></td>
-						<td></td>
-						<td></td>
-						<td></td>
-						<td></td>
-						<td>---</td>
-						<td>---</td>
-						<td></td>
-					</tr>
-					{elseif $meldungen[spieler.index_next].geschlecht ne $meldungen[spieler].geschlecht}
-					<tr>
-						<td></td>
-						<td></td>
-						<td></td>
-						<td></td>
-						<td></td>
-						<td></td>
-						<td>---</td>
-						<td></td>
-					</tr>
-					{/if}
-				  {/section}
+							<td><?php echo $meldungen[$s]['nachname']; ?></td>
+							<td><?php echo $meldungen[$s]['vorname']; ?></td>
+							<td><?php echo $meldungen[$s]['davor']." ".$meldungen[$s]['verein']; ?></td>
+							<td><?php echo $meldungen[$s]['passnummer']; ?></td>
+							<td><?php echo $meldungen[$s]['geburtstag|date_format:"%Y"']; ?></td>
+							<td><?php echo $meldungen[$s]['ak']; ?></td>
+							<td><?php echo $meldungen[$s]['geschlecht']; ?></td>
+							<td><?php echo $meldungen[$s]['partner']; ?></td>
+						</tr><?php 
+					} ?>
 				  </tbody>
 				</table>
-
-?>
+            </div>
+        </div>
+    </section>
