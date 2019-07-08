d337b066e99a0c1e5ed1a84a03cbc6d0d08bbae0
ahhess <andreas.h.hess@googlemail.com>
2019-07-09 00:33:57 +0200

    update vereinsnr auf 05-

--- src/db/tas_vereine-vereinsnr.sql
+++ src/db/tas_vereine-vereinsnr.sql
@@ -3,3 +3,5 @@
 ALTER TABLE `tas_vereine` ADD `vereinsnr` VARCHAR( 10 ) NOT NULL ;
 UPDATE `tas_vereine` SET `vereinsnr` = `id` ;
 ALTER TABLE `tas_vereine` MODIFY `vereinsnr` UNIQUE ;
+update `tas_vereine` set `vereinsnr` = concat('05-', lpad(`vereinsnr`,5,0))
+
